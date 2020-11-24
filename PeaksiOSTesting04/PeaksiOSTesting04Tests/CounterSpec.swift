70c5a4b4b58488790789c74f58b353c2e112ee95
Takahiro Hara <takahiro.hb@gmail.com>
2020-11-25 00:49:32 +0900

    add CountableBehaviorSpec

--- PeaksiOSTesting04/PeaksiOSTesting04Tests/CounterSpec.swift
+++ PeaksiOSTesting04/PeaksiOSTesting04Tests/CounterSpec.swift
@@ -55,3 +55,32 @@ class CounterSpec: QuickSpec {
         }
     }
 }
+
+class CountableBehavior: Behavior<Int> {
+    override static func spec(_ aContext: @escaping () -> Int) {
+        var counter: Counter!
+        var initial: Int!
+        
+        beforeEach {
+            initial = aContext()
+            
+            counter = Counter()
+            counter.reset(initial)
+            counter.increment()
+            counter.increment()
+        }
+        
+        it("incrementを2回呼び出したら、現在の値+2されること") {
+            XCTAssertEqual(counter.count, initial + 2)
+        }
+    }
+}
+
+class CountableBehaviorSpec: QuickSpec {
+    override func spec() {
+        describe("Counter") {
+            itBehavesLike(CountableBehavior.self) { 0 }
+            itBehavesLike(CountableBehavior.self) { 1 }
+        }
+    }
+}
