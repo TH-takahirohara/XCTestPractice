48f5c737622948fca1c2ff6a3baaa6b982b1e8ef
Takahiro Hara <takahiro.hb@gmail.com>
2020-11-26 00:58:38 +0900

    succes first acceptance test

--- BddCounter/BddCounter/CounterViewController.swift
+++ BddCounter/BddCounter/CounterViewController.swift
@@ -13,12 +13,20 @@ class CounterViewController: UIViewController {
     @IBOutlet weak var decrementButton: UIButton!
     @IBOutlet weak var incrementButton: UIButton!
     
+    var count: Int = 0
+    
     static func make() -> CounterViewController {
         let storyboard = UIStoryboard(name: "Counter", bundle: nil)
         return storyboard.instantiateInitialViewController() as! CounterViewController
     }
     
+    @IBAction func tapIncrementButton(_ sender: Any) {
+        count += 1
+        countLabel.text = "\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = "\(count)"
     }
 }
