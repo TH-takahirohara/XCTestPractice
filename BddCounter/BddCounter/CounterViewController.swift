077e4ec2a403a2da88900b4cb9d8c5eb89323182
Takahiro Hara <takahiro.hb@gmail.com>
2020-11-26 00:43:35 +0900

    add CounterViewController

---
+++ BddCounter/BddCounter/CounterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CounterViewController.swift
+//  BddCounter
+//
+//  Created by 原昂大 on 2020/11/26.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
